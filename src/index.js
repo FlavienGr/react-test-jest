f3bb3b0c750a7ead01e5065965df568f9e958779
Flavor85 <flaviengr75@gmail.com>
2019-05-04 15:21:32 +0200

    wired up of redux, commentReducer, actioncreator addComment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import reducers from "./redux/reducers";
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
